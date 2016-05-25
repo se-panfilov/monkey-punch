ca4cee743eae75c3348d1a9c33b8052bf92e59a8
Sergey Panfilov <sp@arex.io>
2016-05-25 19:40:11 +0800

    Refactoring

--- src/index.js
+++ src/index.js
@@ -53,3 +53,87 @@ var monkKeyPatch = (function () {
 
   return exports;
 })();
+
+
+'use strict';
+
+var Monkey = (function (config) {
+
+  if (!config) console.log('error');
+
+  var obj = config.obj;
+  var methodName = config.method;
+  var method = obj[methodName];
+  var methodArr = method.toString().split("\n");
+  var linesCount = methodArr.length;
+
+  //TODO (S.Panfilov) should check all possible valuse that upper than linesCount
+  if (config[linesCount + 1]) console.log('error - string not in func range');
+
+  //TODO (S.Panfilov) should check all possible valuse that lower than 0
+  if (config['-1']) console.log('error - string not in func range');
+
+  var isLinesPatches = false; //TODO (S.Panfilov) should check for keys like 1, 2, 3, etc...
+
+  var fixedFields = ['obj', 'method', 'before', 'after'];
+
+  if (isLinesPatches) {
+    for (var key in config) {
+
+      var isFixedField = false;
+      for (var i = 0; i < fixedFields.length; i++) {
+        var fixedField = fixedFields[i];
+
+
+        if (key === fixedField) {
+          isFixedField = (key === fixedField);
+          break;
+        }
+
+        if (!isFixedField) {
+
+        }
+      }
+
+    }
+  }
+
+  var exports = {
+    patch: function () {
+      //
+    }
+  };
+
+  return exports;
+});
+
+function doItBefore(cb) {
+  console.log('before');
+  if (cb) cb();
+}
+
+function doItAfter(cb) {
+  console.log('after');
+  if (cb) cb();
+}
+
+function doAtFirstLine(cb) {
+  console.log('First(1) line');
+  if (cb) cb();
+}
+
+function doAtFifthLine(cb) {
+  console.log('Fifth(5) line');
+  if (cb) cb();
+}
+
+var sumMonkey = new Monkey({
+  obj: exports,
+  method: 'sum',
+  before: doItBefore,
+  after: doItAfter,
+  1: doAtFirstLine,
+  5: doAtFifthLine
+});
+
+sumMonkey.patch();
