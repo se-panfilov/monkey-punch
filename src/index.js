1cbb716bd6c0342e1ab42af40cdb37f6142415db
Sergey Panfilov <sp@arex.io>
2016-06-29 09:47:41 +0200

    Fixes for positions sorting

--- src/index.js
+++ src/index.js
@@ -32,28 +32,26 @@ var Monkey = (function (config) {
       if (commaIndex === -1) return null;
       return +key.substr(key.indexOf(',') + 1);
     },
+    getColumnAsDecimal: function (line, column) {
+      if (!column && column !== 0) return -(line + '.' +'99999');
+
+      var isLower =column< 0;
+      if (isLower) column *= -1;
+
+      var result = +(line + '.' + column);
+
+      if (isLower) result *= -1;
+
+      return result;
+    },
     getPositionWeight: function (position) {
-      //TODO (S.Panfilov) check issue with array's elements order in JS
-      //var expectedArr = ['0,8', '0,7', '0,-1', '0,-2', '0'];
-      var _this = this;
+      var column = this.getColumnNumber(position);
+      var line = +this.getLineNumber(position);
 
-      // var lines = position.map(function (e) {
-      //   return _this.getLineNumber(e);
-      // });
-      //
-      // var columns = position.map(function (e) {
-      //   //TODO (S.Panfilov) check case for lines only ('6', '9', etc.)
-      //   return _this.getColumnNumber(e);
-      // });
-
-      // return lines - (columns * 0.1);
-      return +(this.getLineNumber(position) + (this.getColumnNumber(position) * 0.1));
+      return line + this.getColumnAsDecimal(line, column);
     },
     sortNumberArr: function (arr) {
       var numberSort = function (a, b) {
-        // return _p.getLineNumber(a) > _p.getLineNumber(b);
-        //TODO (S.Panfilov) test this approach
-        //TODO (S.Panfilov) This works in general, but single values has bad weight ('6', '9', etc.)
         return _p.getPositionWeight(a) < _p.getPositionWeight(b);
       };
 
